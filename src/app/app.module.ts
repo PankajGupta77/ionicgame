b635a41ca867501156dd36360f954c59b21f8763
PankajGupta77 <88587367+PankajGupta77@users.noreply.github.com>
2023-06-20 16:12:34 +0530

    add ad iframe-icon v0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AdMobFree, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },AdMobFree],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdMobFree, AdMobFreeBannerConfig,AdMobFreeInterstitialConfig,AdMobFreeRewardVideoConfig} from '@ionic-native/admob-free/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
-
+  constructor(private admobFree:AdMobFree) {
+    this.showInterstitialAd()
+  }
+  ngOnInit(){
+    this.showInterstitialAd()
+    this.showBannerAd()
+    alert('Please on Portrait')
+  }
+  showBannerAd() {
+    const bannerConfig: AdMobFreeBannerConfig = {
+      id: 'ca-app-pub-7954042482936232/2741183872',
+      isTesting: false,
+      autoShow: true,
+    };
+    this.admobFree.banner.config(bannerConfig);
+    this.admobFree.banner.prepare().then(() => {
+      console.log('Banner ad is ready');
+    });
+  }
+  showInterstitialAd() {
+    const interstitialConfig: AdMobFreeInterstitialConfig = {
+      id: 'ca-app-pub-7954042482936232/4841529761',
+      autoShow: true,
+      isTesting: false
+    };
+    this.admobFree.interstitial.config(interstitialConfig);
+    this.admobFree.interstitial.prepare().then(() => {
+      // interstitial ad is ready to be displayed
+    }).catch((e) => console.log(e));
+  }
 }
