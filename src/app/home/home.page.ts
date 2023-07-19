2d74ca8951176e466f9e0f8c0ba79c389d89fc00
PankajGupta77 <88587367+PankajGupta77@users.noreply.github.com>
2023-07-19 12:23:28 +0530

    admob_working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,43 +9,45 @@ import { Platform } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
   loading: boolean = true;
+  showIframe: boolean = false;
   constructor(private admobFree:AdMobFree,private platform: Platform) {
    
   }
   ngOnInit(){
-    this.showInterstitialAd()
+    //  this.showInterstitialAd()
     // this.showBannerAd()
     // alert('Please on Portrait device first')
   }
-  ionViewDidEnter() {
-    this.platform.ready().then(() => {
-      this.showInterstitialAd();
-    });
-  }
-  showBannerAd() {
-    const bannerConfig: AdMobFreeBannerConfig = {
-      id: 'ca-app-pub-7954042482936232/5086474717',
-      isTesting: false,
-      autoShow: true,
-    };
-    this.admobFree.banner.config(bannerConfig);
-    this.admobFree.banner.prepare().then(() => {
-      console.log('Banner ad is ready');
-    });
-  }
-  showInterstitialAd() {
+ 
+  // ionViewDidLoad(){
+  //   if(this.platform.is('cordova')){
+  //   const bannerConfig: AdMobFreeBannerConfig = {
+  //     id: 'ca-app-pub-7954042482936232/5086474717',
+  //     isTesting: true,
+  //     autoShow: true,
+  //   };
+  //   this.admobFree.banner.config(bannerConfig);
+  //   this.admobFree.banner.prepare().then(() => {
+  //     console.log('Banner ad is ready');
+  //   });
+  // }
+  // }
+  ionViewDidLoad(){
+    if(this.platform.is('cordova')){
     const interstitialConfig: AdMobFreeInterstitialConfig = {
-      id: 'ca-app-pub-7954042482936232/3632526897',
+      id: 'ca-app-pub-7954042482936232/7459101580',
       autoShow: true,
       isTesting: false
     };
     this.admobFree.interstitial.config(interstitialConfig);
     this.admobFree.interstitial.prepare().then(() => {
       // interstitial ad is ready to be displayed
+      console.log("worker is ready")
     }).catch((e) => console.log(e));
   }
+  }
   onIframeLoad() {
- 
+ this.ionViewDidLoad()
     this.loading = false;
   }
   
