c89023719eb31857c4a2cb0c8c51ea67bd9e4e7d
PankajGupta77 <88587367+PankajGupta77@users.noreply.github.com>
2023-09-24 12:57:49 +0530

    done

---
+++ platforms/android/platform_www/plugins/cordova-plugin-vungle/www/Vungle.js
@@ -0,0 +1,15 @@
+cordova.define("cordova-plugin-vungle.Vungle", function(require, exports, module) {
+var Vungle = {
+	setup: function(success_cb, error_cb, debugMode, appId) {
+		cordova.exec(success_cb, error_cb, 'VunglePlugin', 'setup', [debugMode, appId]);
+	},	
+	requestAd: function(success_cb, error_cb){
+		cordova.exec(success_cb, error_cb, 'VunglePlugin', 'requestAd', []);
+	},
+	showAdWithOptions: function(success_cb, error_cb, options){
+		cordova.exec(success_cb, error_cb, 'VunglePlugin', 'showAdWithOptions', [options]);
+	}
+};
+
+module.exports = Vungle;
+});
